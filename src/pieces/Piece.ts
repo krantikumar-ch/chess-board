bf3e00acdc6d00f5608af9197f8a15e43e22a4e0
krantikumar-ch <krantikumar.ch@gmail.com>
2023-05-04 08:17:40 +0530

    Added validations and knight, pawn behavior

--- src/pieces/Piece.ts
+++ src/pieces/Piece.ts
@@ -1,20 +1,58 @@
-export default abstract class Piece{
-    player: number;
-    style:Style;
+import Square from "./Square";
 
-    constructor(player:number, iconUrl:string){
-        this.player = player;
-        this.style = {
-            backgroundColor:'',
-            backgroundImage:"url('"+iconUrl+"')"
-        }
-    }
+export default abstract class Piece {
+  player: number;
+  style: Style;
+  name: string;
 
-    abstract isMovePossible(src:number, dest:number, isDestEnemyOccupied: boolean):boolean;
-    abstract getSrcToDestPath(src:number, dest:number): number[];
+  constructor(name: string, player: number, iconUrl: string) {
+    this.name = name;
+    this.player = player;
+    this.style = {
+      backgroundColor: "",
+      backgroundImage: "url('" + iconUrl + "')",
+    };
+  }
+
+  protected abstract isMovePossible(
+    srcSquare: Square,
+    destSquare: Square,
+    squares: Square[][]
+  ): boolean;
+
+  validateMove(
+    srcSquare: Square,
+    destSquare: Square,
+    squares: Square[][]
+  ): boolean {
+    return (
+      this.isMovePossible(srcSquare, destSquare, squares) &&
+      !this.isSamePlayerAtEnd(destSquare)
+    );
+  }
+
+  protected extractRows(srcSquare: Square, destSquare: Square): any {
+    const srcRow = srcSquare.rowIdx;
+    const srcCol = srcSquare.colIdx;
+    const destRow = destSquare.rowIdx;
+    const destCol = destSquare.colIdx;
+    return { srcRow, srcCol, destRow, destCol };
+  }
+
+  private isSamePlayerAtEnd(destSquare: Square): boolean {
+    return destSquare.piece.player === this.player;
+  }
+
+  highlightBackground(): void {
+    this.style = { ...this.style, backgroundColor: "RGB(111,143,114)" };
+  }
+
+  clearBackground(): void {
+    this.style = { ...this.style, backgroundColor: "" };
+  }
 }
 
-interface Style{
-    backgroundColor: string,
-    backgroundImage: string
+interface Style {
+  backgroundColor: string;
+  backgroundImage: string;
 }
