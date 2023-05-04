af26425be4b75ab00e7f5dd497415eb2194b6fa9
krantikumar-ch <krantikumar.ch@gmail.com>
2023-05-04 09:09:19 +0530

    Added Queen behavior

--- src/pieces/Queen.ts
+++ src/pieces/Queen.ts
@@ -3,15 +3,27 @@ import BlackQueen from "./../assets/black queen.svg";
 import Piece from "./Piece";
 import Square from "./Square";
 import ChessPieces from "../helpers/ChessPieceNames";
+import Bishop from "./Bishop";
+import Rook from "./Rook";
 
-export default class Queen extends Piece{
-    constructor(player:number){
-        super(ChessPieces.QUEEN, player, player == 1 ? WhiteQueen : BlackQueen);
-    }
+export default class Queen extends Piece {
+  bishop: Bishop;
+  rook: Rook;
+  
+  constructor(player: number) {
+    super(ChessPieces.QUEEN, player, player == 1 ? WhiteQueen : BlackQueen);
+    this.bishop = new Bishop(player);
+    this.rook = new Rook(player);
+  }
 
-    isMovePossible(srcSquare: Square, destSquare: Square, squares: Square[][]): boolean {
-        throw new Error("Method not implemented.");
-    }
-    
-    
+  isMovePossible(
+    srcSquare: Square,
+    destSquare: Square,
+    squares: Square[][]
+  ): boolean {
+    return (
+      this.bishop.isMovePossible(srcSquare, destSquare, squares) ||
+      this.rook.isMovePossible(srcSquare, destSquare, squares)
+    );
+  }
 }
